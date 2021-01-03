508ca3ec9a57c47c261cb42fd1d510487143a01d
Markos Baratsas <baratsas.markos@gmail.com>
2021-01-03 19:16:01 +0200

    Add initial page, just to test that everything is working fine
    	Also made some changes in .gitignore to ignore files that we don't need in our repo

--- backend/MHMelectric/rest_api/views.py
+++ backend/MHMelectric/rest_api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_api.models import *
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1>We made it so far!</h1>")
+    
