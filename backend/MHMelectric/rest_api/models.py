4f5a8e780627fdf78c6911936bf92ad7c2f89d08
Markos Baratsas <baratsas.markos@gmail.com>
2021-01-03 19:17:47 +0200

    Add Car_Owner class

--- backend/MHMelectric/rest_api/models.py
+++ backend/MHMelectric/rest_api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Car_Owner(models.Model):
+    owner_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=63, default='')
+    last_name = models.CharField(max_length=63, default='')
+    birthdate = models.DateTimeField(auto_now_add=True) # auto_add now = true and we will change it later...
+    country = models.CharField(max_length=127, default='')
+    city = models.CharField(max_length=127, default='')
+    street = models.CharField(max_length=127, default='')
+    street_number = models.DecimalField(max_digits=4, decimal_places=0, default=0)
+    postal_code = models.DecimalField(max_digits=5, decimal_places=0, default=0)
+    bonus_points = models.DecimalField(max_digits=7, decimal_places=0, default=0)
+    
+    # django provides us with an authentication system which we will use
+    # so we don't have username-password here, since it exists on the User model
+    # We bind this Car_Owner with a specific User instance using a foreign key
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username}'
