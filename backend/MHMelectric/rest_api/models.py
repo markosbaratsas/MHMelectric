2bf8fc376dcaca15378d1d8ed0f560fa3bc3bc35
Markos Baratsas <baratsas.markos@gmail.com>
2021-01-05 19:38:37 +0200

    Make some changes on models and add some extra fields

--- backend/MHMelectric/rest_api/models.py
+++ backend/MHMelectric/rest_api/models.py
@@ -8,7 +8,7 @@ class Car_Owner(models.Model):
     birthdate = models.DateTimeField(auto_now_add=True) # auto_add now = true and we will change it later...
     country = models.CharField(max_length=127, default='')
     city = models.CharField(max_length=127, default='')
-    street = models.CharField(max_length=31, default='')
+    street = models.CharField(max_length=127, default='')
     street_number = models.DecimalField(max_digits=4, decimal_places=0, default=0)
     postal_code = models.DecimalField(max_digits=5, decimal_places=0, default=0)
     bonus_points = models.DecimalField(max_digits=7, decimal_places=0, default=0)
@@ -16,16 +16,15 @@ class Car_Owner(models.Model):
     # django provides us with an authentication system which we will use
     # so we don't have username-password here, since it exists on the User model
     # We bind this Car_Owner with a specific User instance using a foreign key
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING, default=None)
 
     def __str__(self):
-        return str(self.first_name) + str(self.last_name)
-        # return f'{self.user.username}'
-
+        return f'{self.user.username} {self.first_name} {self.last_name}'
 
 
 class Car(models.Model):
     car_id = models.AutoField(primary_key=True)
+    car_id_given = models.CharField(max_length=127, default='')
     brand = models.CharField(max_length=63, default='')
     car_type = models.CharField(max_length=15, default='')
     car_model = models.CharField(max_length=31, default='')
@@ -36,53 +35,53 @@ class Car(models.Model):
     ac_charger_max_power = models.DecimalField(max_digits=4, decimal_places=1, default=0)
     dc_charger_max_power = models.DecimalField(max_digits=4, decimal_places=1, default=0)
     energy_average_consumption = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    average_consumption = models.DecimalField(max_digits=5, decimal_places=2, default=0)
 
-    def __str__(self):
-        return f'{self.car_id}'
-
-
-
-class Car_Owner_Car(models.Model):
-    Car_Owner_Car_id = models.AutoField(primary_key=True)
     owner = models.ForeignKey(Car_Owner, on_delete=models.DO_NOTHING)
-    car = models.ForeignKey(Car, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return str(self.car) + " belongs to " + str(self.owner)
+        return f'{self.car_id}, owned by {owner.first_name}'
 
 
+# class Car_Owner_Car(models.Model):
+#     Car_Owner_Car_id = models.AutoField(primary_key=True)
+#     owner = models.ForeignKey(Car_Owner, on_delete=models.DO_NOTHING)
+#     car = models.ForeignKey(Car, on_delete=models.DO_NOTHING)
 
-class dc_charger_ports(models.Model):
+#     def __str__(self):
+#         return str(self.car) + " belongs to " + str(self.owner)
+
+
+class dc_charger_port(models.Model):
+    dc_charger_port_id = models.AutoField(primary_key=True)
     car = models.ForeignKey(Car, on_delete=models.DO_NOTHING)
-    port = models.CharField(max_length=15, default='', primary_key=True)
+    port = models.CharField(max_length=15, default='')
 
     def __str__(self):
-        return "DC port " + str(self.port) + " of car " + str(self.car)
+        return f'DC port {self.port} of car {self.car}'
 
 
-
-class ac_charger_ports(models.Model):
+class ac_charger_port(models.Model):
+    ac_charger_port_id = models.AutoField(primary_key=True)
     car = models.ForeignKey(Car, on_delete=models.DO_NOTHING)
-    port = models.CharField(max_length=15, default='', primary_key=True)
+    port = models.CharField(max_length=15, default='')
 
     def __str__(self):
-        return "AC port " + str(self.port) + " of car " + str(self.car)
-
+        return f'AC port {self.port} of car {self.car}'
 
 
 class Charging_point(models.Model):
     charging_point_id = models.AutoField(primary_key=True)
     country = models.CharField(max_length=127, default='')
     city = models.CharField(max_length=127, default='')
-    street = models.CharField(max_length=31, default='')
+    street = models.CharField(max_length=127, default='')
     street_number = models.DecimalField(max_digits=4, decimal_places=0, default=0)
     postal_code = models.DecimalField(max_digits=5, decimal_places=0, default=0)
-    phone_number = models.CharField(max_length=31, default='') # why varchar?
-    email = models.CharField(max_length=31, default='')
+    phone_number = models.DecimalField(max_digits=14, decimal_places=0, default=0)
+    email = models.EmailField(max_length=63, default='')
 
     def __str__(self):
         return f'{self.charging_point_id}'
-
 
 
 class Charge_program(models.Model):
@@ -95,15 +94,13 @@ class Charge_program(models.Model):
         return f'{self.charge_program_id}'
 
 
-
 class Charge_program_Charging_point(models.Model):
     charge_program_charging_point_id = models.AutoField(primary_key=True)
     charge_program = models.ForeignKey(Charge_program, on_delete=models.DO_NOTHING)
     charging_point = models.ForeignKey(Charging_point, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return "Charging point " + str(self.charging_point) + " has charge program " + str(self.charge_program)
-
+        return f'Charging point {self.charging_point} has charge program {self.charge_program}'
 
 
 class Provider(models.Model):
@@ -111,14 +108,13 @@ class Provider(models.Model):
     title = models.CharField(max_length=127, default='')
     website_url = models.CharField(max_length=127, default='')
     comments = models.CharField(max_length=1023, default='')
-    primary_phone = models.CharField(max_length=31, default='') # why varchar?
-    secondary_phone = models.CharField(max_length=31, default='') # why varchar?
+    primary_phone = models.DecimalField(max_digits=14, decimal_places=0, default=0)
+    secondary_phone = models.DecimalField(max_digits=14, decimal_places=0, default=0)
     address_info = models.CharField(max_length=127, default='')
-    email = models.CharField(max_length=63, default='')
+    email = models.EmailField(max_length=63, default='')
 
     def __str__(self):
         return f'{self.provider_id}'
-
 
 
 class Charging_point_Provider(models.Model):
@@ -127,17 +123,18 @@ class Charging_point_Provider(models.Model):
     charging_point = models.ForeignKey(Charging_point, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return "Provider " + str(self.provider) + " in charging point " + str(self.charging_point)
-
+        return f'Provider {self.provider} in charging point {self.charging_point}'
 
 
 class Connection_type(models.Model):
     connection_type_id = models.AutoField(primary_key=True)
     connection_type = models.CharField(max_length=31, default='')
+    general_type = models.CharField(max_length=31, default='')
+    voltage = models.DecimalField(max_digits=4, decimal_places=0, default=0)
+    amps = models.DecimalField(max_digits=3, decimal_places=0, default=0)
     
     def __str__(self):
-        return self.connection_type
-
+        return f'{self.connection_type}'
 
 
 class Charging_point_Connection_type(models.Model):
@@ -146,8 +143,7 @@ class Charging_point_Connection_type(models.Model):
     charging_point = models.ForeignKey(Charging_point, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return "Connection type " + str(self.connection_type) + " in charging point " + str(self.charging_point)
-
+        return f'Connection type {self.connection_type} in charging point {self.charging_point}'
 
 
 class Periodic_bill(models.Model):
@@ -155,27 +151,27 @@ class Periodic_bill(models.Model):
     owner = models.ForeignKey(Car_Owner, on_delete=models.DO_NOTHING)
     paid = models.BooleanField(default=False)
     published_on = models.DateTimeField(auto_now_add=True)
-    total = models.DecimalField(max_digits=16, decimal_places=12, default=0)
-    discount = models.DecimalField(max_digits=16, decimal_places=12, default=0)
+    total = models.DecimalField(max_digits=16, decimal_places=2, default=0)
+    discount = models.DecimalField(max_digits=16, decimal_places=2, default=0)
 
     def __str__(self):
         return f'{self.periodic_bill_id}'
 
 
-
 class Session(models.Model):
     session_id = models.AutoField(primary_key=True)
+    session_id_given = models.CharField(max_length=255, default='')
     car = models.ForeignKey(Car, on_delete=models.DO_NOTHING)
     car_owner = models.ForeignKey(Car_Owner, on_delete=models.DO_NOTHING)
     charging_point = models.ForeignKey(Charging_point, on_delete=models.DO_NOTHING)
-    periodic_bil = models.ForeignKey(Periodic_bill, on_delete=models.DO_NOTHING)
+    periodic_bil = models.ForeignKey(Periodic_bill, on_delete=models.DO_NOTHING, default=None)
     connection_time = models.DateTimeField(auto_now_add=True)
-    disconnection_time = models.DateTimeField(auto_now_add=True)
-    done_charging_time = models.DateTimeField(auto_now_add=True)
-    kWh_delivered = models.DecimalField(max_digits=5, decimal_places=3, default=0)
+    disconnection_time = models.DateTimeField(auto_now_add=True) # auto_now_add=True and it will change later
+    done_charging_time = models.DateTimeField(auto_now_add=True) # auto_now_add=True and it will change later
+    kWh_delivered = models.DecimalField(max_digits=8, decimal_places=3, default=0)
     timezone = models.CharField(max_length=31, default='')
     user_Wh_per_mile = models.DecimalField(max_digits=4, decimal_places=0, default=0)
-    user_Wh_requested = models.DecimalField(max_digits=5, decimal_places=3, default=0)
+    user_kWh_requested = models.DecimalField(max_digits=8, decimal_places=3, default=0)
     user_miles_requested = models.DecimalField(max_digits=4, decimal_places=0, default=0)
     user_minutes_available = models.DecimalField(max_digits=4, decimal_places=0, default=0)
     user_modified_at = models.DateTimeField(auto_now_add=True)
