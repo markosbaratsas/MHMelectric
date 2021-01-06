0caadccd46193d18895f8d5c73fc4e0e0d3fc452
Markos Baratsas <baratsas.markos@gmail.com>
2021-01-06 22:55:51 +0200

    Add functionality for user registration and an initial api-view

--- backend/MHMelectric/rest_api/urls.py
+++ backend/MHMelectric/rest_api/urls.py
@@ -4,4 +4,6 @@ import rest_api.views as views
 
 urlpatterns = [
     path('', views.index),
+    path('get_first_car/', views.get_first_car, name="get_first_car"),
+    path('register', views.register_user, name="register_user"),    
 ]
