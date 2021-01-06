e2b5bb0d55f6798b5c57712fa1613a283f7f2b55
Markos Baratsas <baratsas.markos@gmail.com>
2021-01-06 22:53:07 +0200

    Install rest_framework, which we will be used for the CLI mainly

--- backend/MHMelectric/MHMelectric/settings.py
+++ backend/MHMelectric/MHMelectric/settings.py
@@ -39,7 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_api.apps.RestApiConfig',
+    'users.apps.UsersConfig',
+
+    'rest_framework',
+    'rest_framework.authtoken'    
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
