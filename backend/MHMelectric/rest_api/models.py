0cc5c06a53e0828900f30cf952081eec9cd1b8c4
Markos Baratsas <baratsas.markos@gmail.com>
2021-03-12 22:09:07 +0200

    Add default value for country

--- backend/MHMelectric/rest_api/models.py
+++ backend/MHMelectric/rest_api/models.py
@@ -7,7 +7,7 @@ class Car_Owner(models.Model):
     first_name = models.CharField(max_length=63, default='')
     last_name = models.CharField(max_length=63, default='')
     birthdate = models.DateTimeField(default=datetime.datetime(1966, 8, 8, 0, 0, 0, 0), blank=True) # auto_add now = true and we will change it later...
-    country = models.CharField(max_length=127, default='')
+    country = models.CharField(max_length=127, default='Not specified')
     city = models.CharField(max_length=127, default='')
     street = models.CharField(max_length=127, default='')
     street_number = models.DecimalField(max_digits=4, decimal_places=0, default=0)
