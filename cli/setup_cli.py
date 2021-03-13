92044a94c9bed8246d45b28c17a8e2c66b7a8388
IlianaXn <el17059@mail.ntua.gr>
2021-03-13 22:34:24 +0200

    Fix bug in setup_cli.py

--- cli/setup_cli.py
+++ cli/setup_cli.py
@@ -5,9 +5,10 @@ import os
 if __name__ == '__main__':
     path = str(pathlib.Path(__file__).parent.absolute())
     os.system("chmod +x ev_group23")
-    os.system(f"cp {path}/ev_group23 {path}/venv/bin/ev_group23")
     with in_place.InPlace(path+"/ev_group23") as file:
         for line in file:
             if 'python3' in line:
                 line = '    python3 '+ path+ '/cli_side.py ${args}\n'
             file.write(line)
+
+    os.system(f"cp {path}/ev_group23 {path}/venv/bin/ev_group23")
