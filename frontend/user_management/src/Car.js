428cf8e2122cce307303c62617a05c71888d93de
mariartc <maraki.rtc@hotmail.com>
2021-03-13 16:20:10 +0200

    Add option to change user info and add car

--- frontend/user_management/src/Car.js
+++ frontend/user_management/src/Car.js
@@ -2,11 +2,59 @@ import './css/Login.css'
 import './css/Header.css'
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Modal from 'react-modal'
 
 function Car() {
     const [ car, setCar ] = useState([])
+    const [ modalIsOpen, setModalIsOpen ] = useState(false)
+    const [ carAdd, setCarAdd ] = useState({})
 
+    function openModal() {
+        setModalIsOpen(true)
+        setCarAdd({})
+    }
 
+    const modalStyle = {
+        overlay: {
+          position: 'fixed',
+          backdropFilter: 'blur(3px)',
+          backgroundColor: 'rgba(255, 255, 255, 0.1)'
+        },
+        content: { 
+          maxHeight: '76%',
+          overflowY: 'auto',
+          top: '12%',
+          left: '33%',
+          right: 'auto',
+          bottom: 'auto',
+          position: 'absolute', 
+          width: '34%', 
+          borderWidth: '1px',
+          paddingLeft: '30px',
+          paddingTop: '0px'
+        }}
+
+    const handleChangeCar = (e) => {
+        const { value, name } = e.target
+        setCarAdd({...carAdd, [name]: value})
+        console.log(carAdd)
+    }
+
+    function submitCar() {
+        var details = {
+            method: 'post',
+            url: 'http://127.0.0.1:8765/evcharge/api/add_car',
+            headers: {
+            'X-OBSERVATORY-AUTH': JSON.parse(localStorage["tokens"])
+            },
+            data: carAdd
+        }
+        axios(details)
+            .catch( (error) => {
+                alert(error)
+                console.log(error)
+            })
+    }
 
     useEffect(() => {
         if (localStorage.getItem("cars")===null) {
@@ -66,7 +114,56 @@ function Car() {
                 <hr />
             </>
             )}
+            <div className='profile-center'>
+                <button onClick={openModal} className='basic-button waves-effect waves-light btn'>Add car</button>
+            </div>
         </div>
+
+        <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} style={modalStyle}>
+        <form className='profile-div inside-modal-div' onSubmit={submitCar}>
+                <div className='profile-center'>
+                    <h3>Type the information of your car</h3>
+                    <hr />
+                </div>
+                <div className='modal-row first'>
+                    <div className='modal-input-div'>
+                        <label htmlFor="brand">Type brand*:</label>
+                        <input type='brand' name='brand' className='input-city-select modal-input-div' onChange={handleChangeCar} required />
+                    </div>
+                </div>
+                <div className='modal-row'>
+                    <div className='modal-input-div'>
+                        <label htmlFor="car_type">Type type*:</label>
+                        <input type='car_type' name='car_type' className='input-city-select modal-input-div' onChange={handleChangeCar} required />
+                    </div>
+                </div>
+                <div className='modal-row'>
+                    <div className='modal-input-div'>
+                        <label htmlFor="car_model">Type model*:</label>
+                        <input type='car_model' name='car_model' className='input-city-select modal-input-div' onChange={handleChangeCar} required />
+                    </div>
+                </div>
+                <div className='modal-row'>
+                    <div className='modal-input-div'>
+                        <label htmlFor="variant">Type variant*:</label>
+                        <input type='variant' name='variant' className='input-city-select modal-input-div' onChange={handleChangeCar} required />
+                    </div>
+                </div>
+                <div className='modal-row'>
+                    <div className='modal-input-div left'>
+                        <label htmlFor="release_year">Type release year*:</label>
+                        <input type='number' name='release_year' className='input-city-select modal-input-div' onChange={handleChangeCar} required />
+                    </div>                
+                    <div className='modal-input-div'>
+                        <label htmlFor="usable_battery_size">Type usable battery size:</label>
+                        <input type='number' name='usable_battery_size' className='input-city-select modal-input-div' onChange={handleChangeCar}/>
+                    </div>
+                </div>
+                <div className='profile-center'>
+                    <button type='submit' className='basic-button waves-effect waves-light btn modal-btn'>Add car</button>
+                </div>
+            </form>             
+        </Modal>
         </>
     );
 }
--- frontend/user_management/src/Profile.js
+++ frontend/user_management/src/Profile.js
@@ -4,53 +4,95 @@ import { useAuth } from './context/auth'
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import profile from './images/profile-pic.png'
+import Modal from 'react-modal'
 
 function Profile() {
     const { setAuthTokens } = useAuth();
     const [ user, setUser ] = useState({})
+    const [ modalIsOpen, setModalIsOpen ] = useState(false)
+    const [ userEdit, setUserEdit ] = useState({})
 
     function logOut() {
         localStorage.clear();
         setAuthTokens();
     }
 
-    useEffect(() => {
-        if (localStorage.getItem("username")===null) {
-            var details = {
-                method: 'get',
-                url: 'http://127.0.0.1:8765/evcharge/api/get_user_info',
-                headers: {
-                'X-OBSERVATORY-AUTH': JSON.parse(localStorage["tokens"])
-                }
-            }
-            axios(details)
-                .then( (response) => {
-                localStorage.setItem("username", response.data["username"])
-                localStorage.setItem("email", response.data["email"])
-                localStorage.setItem("owner_id", response.data["car_owner"]["owner_id"])
-                localStorage.setItem("first_name", response.data["car_owner"]["first_name"])
-                localStorage.setItem("last_name", response.data["car_owner"]["last_name"])
-                localStorage.setItem("birthdate", response.data["car_owner"]["birthdate"].substring(0, 10))
-                localStorage.setItem("country", response.data["car_owner"]["country"])
-                localStorage.setItem("city", response.data["car_owner"]["city"])
-                localStorage.setItem("street", response.data["car_owner"]["street"])
-                localStorage.setItem("street_number", response.data["car_owner"]["street_number"])
-                localStorage.setItem("postal_code", response.data["car_owner"]["postal_code"])
-                localStorage.setItem("bonus_points", response.data["car_owner"]["bonus_points"])
-                setUser({"username": localStorage["username"], "email": localStorage["email"], "first_name": localStorage["first_name"], 
-                "last_name": localStorage["last_name"], "birthdate": localStorage["birthdate"], 
-                "country": localStorage["country"], "city": localStorage["city"], "street": localStorage["street"], 
-                "street_number": localStorage["street_number"], "postal_code": localStorage["postal_code"], "bonus_points": localStorage["bonus_points"]})
-                })
-                .catch( (error) => {
-                    console.log(error)
-                })
-        } else {
-            setUser({"username": localStorage["username"], "email": localStorage["email"], "first_name": localStorage["first_name"], 
-                "last_name": localStorage["last_name"], "birthdate": localStorage["birthdate"], 
-                "country": localStorage["country"], "city": localStorage["city"], "street": localStorage["street"], 
-                "street_number": localStorage["street_number"], "postal_code": localStorage["postal_code"], "bonus_points": localStorage["bonus_points"]})
+    function submitUser() {
+        var details = {
+            method: 'post',
+            url: 'http://127.0.0.1:8765/evcharge/api/change_user_info',
+            headers: {
+            'X-OBSERVATORY-AUTH': JSON.parse(localStorage["tokens"])
+            },
+            data: {...userEdit, "birthdate": userEdit["birthdate"] + ' 00:00:00'}
         }
+        axios(details)
+            .catch( (error) => {
+                alert(error)
+                console.log(error)
+            })
+    }
+
+    const modalStyle = {
+        overlay: {
+          position: 'fixed',
+          backdropFilter: 'blur(3px)',
+          backgroundColor: 'rgba(255, 255, 255, 0.1)'
+        },
+        content: { 
+          maxHeight: '76%',
+          overflowY: 'auto',
+          top: '12%',
+          left: '33%',
+          right: 'auto',
+          bottom: 'auto',
+          position: 'absolute', 
+          width: '34%', 
+          borderWidth: '1px',
+          paddingLeft: '30px',
+          paddingTop: '0px'
+        }}
+
+    function openModal() {
+        setUserEdit(user)
+        setModalIsOpen(true)
+    }
+
+    const handleChangeUser = (e) => {
+        const { value, name } = e.target
+        setUserEdit({...userEdit, [name]: value})
+    }
+
+    useEffect(() => {
+        var details = {
+            method: 'get',
+            url: 'http://127.0.0.1:8765/evcharge/api/get_user_info',
+            headers: {
+            'X-OBSERVATORY-AUTH': JSON.parse(localStorage["tokens"])
+            }
+        }
+        axios(details)
+            .then( (response) => {
+            localStorage.setItem("username", response.data["username"])
+            localStorage.setItem("email", response.data["email"])
+            localStorage.setItem("owner_id", response.data["car_owner"]["owner_id"])
+            localStorage.setItem("first_name", response.data["car_owner"]["first_name"])
+            localStorage.setItem("last_name", response.data["car_owner"]["last_name"])
+            localStorage.setItem("birthdate", response.data["car_owner"]["birthdate"].substring(0, 10))
+            localStorage.setItem("country", response.data["car_owner"]["country"])
+            localStorage.setItem("city", response.data["car_owner"]["city"])
+            localStorage.setItem("street", response.data["car_owner"]["street"])
+            localStorage.setItem("street_number", response.data["car_owner"]["street_number"])
+            localStorage.setItem("postal_code", response.data["car_owner"]["postal_code"])
+            localStorage.setItem("bonus_points", response.data["car_owner"]["bonus_points"])
+            setUser({"username": localStorage["username"], "email": localStorage["email"], "first_name": localStorage["first_name"], 
+            "last_name": localStorage["last_name"], "birthdate": localStorage["birthdate"], 
+            "country": localStorage["country"], "city": localStorage["city"], "street": localStorage["street"], 
+            "street_number": localStorage["street_number"], "postal_code": localStorage["postal_code"], "bonus_points": localStorage["bonus_points"]})
+            })
+            .catch( (error) => {
+                console.log(error)
+            })
     }, [])
 
     return (
@@ -86,10 +128,69 @@ function Profile() {
                 <h1>{user["bonus_points"]}</h1> 
             </div>
             <hr />
-            <div className='profile-center'>
+            <div className='profile-center account-buttons'>
+                <button onClick={openModal} className='basic-button waves-effect waves-light btn edit'>Edit account</button>
                 <button onClick={logOut} className='basic-button waves-effect waves-light btn'>Log out</button>
             </div>
         </div>
+
+        <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} style={modalStyle}>
+            <form className='profile-div inside-modal-div' onSubmit={submitUser}>
+                <div className='profile-center'>
+                    <h3>Type the information you want to edit</h3>
+                    <hr />
+                </div>
+                <div className='modal-row first'>
+                    <div className='modal-input-div left'>
+                        <label htmlFor="first_name">Edit first name:</label>
+                        <input type='first_name' value={userEdit["first_name"]} name='first_name' className='input-city-select modal-input-div' onChange={handleChangeUser} required />
+                    </div>
+                    <div className='modal-input-div'>
+                        <label htmlFor="first_name">Edit last name:</label>
+                        <input type='last_name' value={userEdit["last_name"]} name='last_name' className='input-city-select modal-input-div' onChange={handleChangeUser} required />
+                    </div>
+                </div>
+                <div className='modal-row'>
+                    <div className='modal-input-div'>
+                        <label htmlFor="email">Edit email:</label>
+                        <input type='email2' value={userEdit["email"]} name='email' className='input-city-select modal-input-div' onChange={handleChangeUser} required />
+                    </div>
+                </div>
+                <div className='modal-row'>
+                    <div className='modal-input-div'>
+                        <label htmlFor="birthdate">Edit birthdate in yyyy-mm-dd format:</label>
+                        <input type='birthdate' value={userEdit["birthdate"]} name='birthdate' className='input-city-select modal-input-div' onChange={handleChangeUser} required />
+                    </div>
+                </div>
+                <div className='modal-row'>
+                    <div className='modal-input-div left street'>
+                        <label htmlFor="street">Edit street name:</label>
+                        <input type='street' value={userEdit["street"]} name='street' className='input-city-select modal-input-div' onChange={handleChangeUser} required />
+                    </div>
+                     <div className='modal-input-div left'>
+                        <label htmlFor="street_number">Edit street number:</label>
+                        <input type='number' value={userEdit["street_number"]} name='street_number' className='input-city-select modal-input-div' onChange={handleChangeUser} required />
+                    </div>
+                    <div className='modal-input-div'>
+                        <label htmlFor="postal_code">Edit postal code:</label>
+                        <input type='number' value={userEdit["postal_code"]} name='postal_code' className='input-city-select modal-input-div' onChange={handleChangeUser} required />
+                    </div>
+                </div>
+                <div className='modal-row'>
+                    <div className='modal-input-div left'>
+                        <label htmlFor="city">Edit city:</label>
+                        <input type='city' value={userEdit["city"]} name='city' className='input-city-select modal-input-div' onChange={handleChangeUser} required />
+                    </div>
+                    <div className='modal-input-div'>
+                        <label htmlFor="country">Edit country:</label>
+                        <input type='country' value={userEdit["country"]} name='country' className='input-city-select modal-input-div' onChange={handleChangeUser} required />
+                    </div>
+                </div>
+                <div className='profile-center'>
+                    <button type='submit' className='basic-button waves-effect waves-light btn modal-btn'>Submit changes</button>
+                </div>
+            </form>                
+       </Modal>
         </>
     );
 }
